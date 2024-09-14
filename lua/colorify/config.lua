896355f14e327b14682a68675cdb53bc38ac5abe
server <server@MANIKs-iMac.local>
2024-09-14 16:21:04 +0600

    bruh

--- lua/colorify/config.lua
+++ lua/colorify/config.lua
@@ -3,14 +3,14 @@ local M = {}
 M.options = {
 	colorify = {
 		enabled = true,
-		mode = "virtual", -- fg, bg, virtual
+		mode = "bg", -- fg, bg, virtual
 		virt_text = "󱓻 ",
 		highlight = { hex = true, lspvars = true },
 	},
 	cmp = {
 		format_colors = {
 			tailwind = true, -- will work for css lsp too
-			icon = "󱓻",
+			icon = "",
 		},
 	},
 }
