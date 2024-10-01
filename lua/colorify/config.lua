cca4d009888af9595c4d6becf3d32275d893a425
arztareef <thetareef4rz@skiff.com>
2024-10-02 00:03:22 +0600

    Supportation

--- lua/colorify/config.lua
+++ lua/colorify/config.lua
@@ -2,14 +2,14 @@ local options = {
 	colorify = {
 		enabled = true,
 		mode = "virtual", -- fg, bg, virtual
-		virt_text = "󱓻 ",
+		virt_text = " ",
 		highlight = { hex = true, lspvars = true },
 	},
 
 	cmp = {
 		format_colors = {
 			tailwind = true, -- will work for css lsp too
-			icon = "󱓻",
+			icon = "",
 		},
 	},
 }
