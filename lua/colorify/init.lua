1d903591459d34dcbbc5ae561cfe3e858cb63851
arztareef <thetareef4rz@skiff.com>
2024-10-19 16:24:14 +0600

    updates and shi idk

--- lua/colorify/init.lua
+++ lua/colorify/init.lua
@@ -1,6 +1,9 @@
 local M = {}
 local api = vim.api
 
+local state = require("colorify.state")
+state.ns = api.nvim_create_namespace("Colorify")
+
 M.attach = require("colorify.attach")
 
 M.run = function()
@@ -13,17 +16,12 @@ M.run = function()
 		"WinScrolled",
 		"BufEnter",
 	}, {
-		-- callback = function(args)
 		callback = function(args)
 			if vim.bo[args.buf].bl then
 				M.attach(args.buf, args.event)
 			end
 		end,
 	})
-end
-
-M.cmp_attach = function(e, i)
-	require("colorify.format").tailwind(e, i)
 end
 
 return M
--- lua/colorify/utils.lua
+++ lua/colorify/utils.lua
@@ -2,6 +2,7 @@ local M = {}
 local api = vim.api
 local get_extmarks = api.nvim_buf_get_extmarks
 local conf = require("colorify.config").colorify
+local ns = require("colorify.state").ns
 
 function M.is_dark(hex)
 	hex = hex:gsub("#", "")
@@ -12,7 +13,7 @@ function M.is_dark(hex)
 	return brightness < 128
 end
 
-function M.add_hl(ns, hex)
+function M.add_hl(hex)
 	local name = "hex_" .. hex:sub(2)
 
 	local fg, bg = hex, hex
@@ -23,22 +24,20 @@ function M.add_hl(ns, hex)
 		bg = "none"
 	end
 
-	api.nvim_set_hl(ns, name, { fg = fg, bg = bg })
+	api.nvim_set_hl(0, name, { fg = fg, bg = bg, default = true })
 	return name
 end
 
-function M.not_colored(buf, ns, linenr, col, hl_group, opts)
+function M.not_colored(buf, linenr, col, hl_group, opts)
 	local ms = get_extmarks(buf, ns, { linenr, col }, { linenr, opts.end_col }, { details = true })
-	ms = #ms == 0 and {} or ms[1]
 
-	local old_hl
-
-	if #ms > 0 then
-		opts.id = ms[1]
-		old_hl = ms[4].hl_group or ms[4].virt_text[1][2]
+	if #ms == 0 then
+		return true
 	end
 
-	return #ms == 0 or old_hl ~= hl_group
+	ms = ms[1]
+	opts.id = ms[1]
+	return hl_group ~= (ms[4].hl_group or ms[4].virt_text[1][2])
 end
 
 return M
